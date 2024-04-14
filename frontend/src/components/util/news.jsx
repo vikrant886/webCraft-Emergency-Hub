7fdf74381c7ddcab3c20243b54f14dfeaa6be2aa
Vikrant Rana <vikrantrana817@gmail.com>
2024-04-14 23:16:09 +0530

    api change

--- frontend/src/components/util/news.jsx
+++ frontend/src/components/util/news.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getNews = async () => {
     try {
-        const response = await axios.get("https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=7447d46e964447f9b0ba9722c19e6d69");
+        const response = await axios.get("https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=8d6eb5d2e1144701b4aab610f58b5651");
         const data = response.data;
         return data;
     } catch (error) {
