7dcc3ea751eae1219e548e626ba815ec35808af3
Vikrant rana <94632119+vikrant886@users.noreply.github.com>
2024-04-15 23:25:33 +0530

    Update news.jsx

--- frontend/src/components/util/news.jsx
+++ frontend/src/components/util/news.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 export const getNews = async () => {
     try {
         const response = await axios.get("https://newsapi.in/newsapi/news.php?key=3rlbN4wMDHLonkpSI2ZgnKrCI0WDC0&category=hindi_state");
+        console.log(resonse)
         const data = response.data;
         return data;
     } catch (error) {
